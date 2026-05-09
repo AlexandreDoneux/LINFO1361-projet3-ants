3bf54390ba8ba585f2d4abe1777ed7a2dcb4647a
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2026-05-09 18:26:41 +0200

    Rework comments on how implementing + function signatures

--- strategies/cooperative.py
+++ strategies/cooperative.py
@@ -25,3 +25,16 @@ class CooperativeStrategy(AntStrategy):
 
         random_direction = random.choice([AntAction.MOVE_FORWARD, AntAction.TURN_LEFT, AntAction.TURN_RIGHT])
         return random_direction  # Random movement for now, replace with actual logic
+
+
+
+    # Cooperative strategy :
+
+     # Si on utilise des phéromones ils doivent decay à chaque pas fait. Sinon on ne peut pas prévoir la direction de la
+    # piste. DOnc ce serait une itération/pas par action.
+
+    # On pourrait utiliser les phéromones pour indiquer qu'il n'y ait plus de nourriture si on fait en sorte de
+    # récupérer un max de nouriture dans la zone avant de revenir. Mais en même temps, ces phéromones peuvent permettre
+    # de demander de l'aide à d'autres fourmis. SI il y a de la nouriture par tas c'est clairement plus intéressant.
+
+    # Mieux vaut revenir directement à la colonie dès qu'on a eu une info de nouriture vu qu'on peut communiquer cette info à d'autres via les phéromones
--- strategies/non_cooperative.py
+++ strategies/non_cooperative.py
@@ -8,6 +8,16 @@ class NonCooperativeStrategy(AntStrategy):
     """
     # TODO: Insert your code here
     """
+    # store for each ant :
+    # - position de la colonie
+    # - position de la nouriture vue mais pas encore récupérée
+    # - position des endroits déjà vus ? => beaucoup de données à stocker
+    # - l'action particulière qu'il fait
+    #      - se déplace à un entroit particulier (goto) + position
+    #      - faire un scan de la zone (radar) + position/info de ce scan => voir techniques dans le cours
+    #      - faire une stratégie de recherche (scatter) + position/info de ce scatter => voir techniques dans le cours
+    #
+    # - position des nourritures récupérées et déjà récupérées ? => pour approfondir la recherche dans cette zone
 
     def __init__(self):
         """Initialize the strategy with last action tracking"""
@@ -24,45 +34,65 @@ class NonCooperativeStrategy(AntStrategy):
         """Decide which direction to move based on current state"""
         # TODO: Insert your code here
 
+        # won't be used in the agent we implement
+
         random_direction = random.choice([AntAction.MOVE_FORWARD, AntAction.TURN_LEFT, AntAction.TURN_RIGHT])
         return random_direction  # Random movement for now, replace with actual logic
 
 
-    # doit-on utiliser _decide_movement() ? non, c'est une méthode privée et est utilisée que dans decide_action()
+    def goto(self, position, destination):
+        """Move towards a specific position"""
+
+        # => voir algorithmes de pathfinding
+        # -> warning : obstacles !
+
+        pass
+
+
+    def scan(self):
+        """Scan the surrounding area for food"""
+
+        # take into account the cells the ant already saw to avoid scanning the same area again
+        # many ways to improve that scan by storing different values ?
+
+        # first version : simple 360° scan around the ant
+
+        pass
+
+    def scatter(self):
+        """Randomly explore the environment when no information is available"""
+
+        # possibility to search randomly or using a more efficient method like a spiral, zigzag, other pattern ?
+
+        pass
+
+
+    def closest_food(self):
+        """Move towards the closest food source seen"""
+
+        # On peut se souvenir de la nourriture qu'on aurait vu. Imaginons qu'on voit deux nouritures et qu'en se
+        # déplaçant vers l'une on perds de vue l'autre. On peut s'en souvenir pour la rejoindre par après.
+
+        # => attention : obstacles ! => dans une verion améliorée
+
+        pass
+
+
 
     # WARNING : un pas est défini par une action (decide_action).
-    # Est-ce qu'on peut faire un goto() ou radar() au niveau de notre stratégie sans trigger decide_action à chaque pas ?
-    # Est-ce qu'à chaque action (déplacement, rotation, récupération, dépot, ect.) decide_action() sera appelé ?
-    # Ou bien est-ce qu'un appel à decide_action() correspond à une décision, une itération ?
+    # On ne peut pas faire plusieurs actions par tour. decide_action() doit renvoyer une action unique
+    # (avancer, rotation, pickup, drop, déposer un type de pheromones, ne rien faire)
 
     # radius de vision : 3, pas 4 !
 
-    # Étapes
-    # - stocker la position de la colonie
-    # - implémenter radar() et récupération de la nouriture trouvée
-    # - implémenter closest_food() pour trouver la nouriture la plus proche (pour optimiser le déplacement vers celle-ci)
-    # - implémenter goto() pour se déplacer vers un endroit précis (colonie ou nouriture)
-    # - améliorer radar() pour si il trouve de la nouriture commencer à regarder autour (déplacement autour de la zone ananlysée/où la nourriture est trouvée)
-    # - implémenter scatter() pour faire une recherche aléatoire à partir d'un endroit (colonie ou zone de nouriture trouvée).
-    # - implémenter decide_action() pour définir le comportement de la fourmi
-    # - stocker la nourriture trouvée avec des zones de libres à proximité (disons une zone de 2 fois son radius de vision)
-    # - Prendre en compte les obstacles dans goto(), scatter(), closest_food()
-    #
-    # => voir après
+    # Étapes :
+    # à redéterminer
 
 
 
     # Non-cooperative strategy:
     # aucun partage d'information mais on a une mémoire
 
-    # mémoire :
-    # - position de la colonie
-    # - position de la nouriture vue mais pas encore récupérée. Le temps de s'y déplacer et la récupérer.
-    #       => lors de la récupération peut potentiellement se souvenir de la position d'ancienne nouriturre avec encore des endroits possibles
-    #      => quand enlever ces positions-là de la mémoire ?
-    # - stocker les endroits où on a trouvé de la nourriture et où il y a des espaces encore pour chercher. Si une position
-    #  de nouriture n'a pas d'espace innexploré à proximité on peut l'oublier.
-    # - Les cellules déjà "vues" pour favoriser la recherche à d'autres endoroits (mais rien n'empêche une nouriture d'avoir été redéplacé vers un endorit déjà vu.)
 
     # ATTENTION :
     # Une fourmi peut porter autant de nouriture qu'elle veut. Il ne semble pas y avoir de limite. Mieux vaut chercher
@@ -70,32 +100,10 @@ class NonCooperativeStrategy(AntStrategy):
 
     # Il n'est pas possible qu'un nouriture soit mis à un autre endroit que la colonie. Il n'y aura donc jamais de nouriture à un ednroit où il n'y en avait pas jusque là.
 
-
     # A-t-on une info du nombre de nouriture totale sur le terrain ? Quand est-ce que le jeu s'arete-t-il ?
     # temps et nombre de pas limités. Si 90% de la nouriture a été trouvée dans ce temps là on considére que la stratégie réussis.
 
 
-    # SI on peut changer le comportement de l'agent
-
-    # goto()
-    # -> ordonner à une fourmi d'aller à un endroit précis
-    # => voir algorithmes de pathfinding
-    # -> warning : obstacles !
-
-    # scatter()
-    # -> stratégie de recherche lorsqu'on a aucune info
-    # => pour chercher de la nouriture ou des phéromones dans les agents 2 et 3
-
-
-    # closest_food()
-    # -> si on se souvient de nouriture on doit trouver la nouriture la plus proche.
-    # -> attention : obstacles ! => dans une verion améliorée
-
-
-    # radar()
-    # -> recherche de la nouriture ou des phéromones autour de soi
-    # -> avec simple tour 360° autour de la fourmi ?
-    # -> avec recherche un peu plus éfficace ? intensification (rechercher dans la même zone) ou diversification (scatter) selon certaines probabilités ?
 
 
 
